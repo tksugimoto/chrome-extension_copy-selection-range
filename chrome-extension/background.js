495e4fd25c22f9cdf2b5e6c4a97d4b2981b37e08
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2017-01-26 18:41:13 +0900

    [Add] 選択範囲のhtmlをコピーするChrome拡張

---
+++ chrome-extension/background.js
@@ -0,0 +1,38 @@
+
+const CONTEXT_MENU_ID_COPY_HTML = "a";
+
+function createContextMenus() {
+	chrome.contextMenus.create({
+		title: "選択範囲のHTMLをコピー",
+		contexts: ["selection"],
+		id: CONTEXT_MENU_ID_COPY_HTML
+	});
+}
+
+chrome.runtime.onInstalled.addListener(createContextMenus);
+chrome.runtime.onStartup.addListener(createContextMenus);
+
+chrome.contextMenus.onClicked.addListener(info => {
+	if (info.menuItemId === CONTEXT_MENU_ID_COPY_HTML) {
+		chrome.tabs.executeScript({
+			frameId: info.frameId,
+			file: "/copy_html.js"
+		});
+	}
+});
+
+chrome.runtime.onMessage.addListener(({method, value}) => {
+	if (method === "copy") {
+		copy(value);
+	}
+});
+
+const copy = text => {
+	let textarea = document.createElement("textarea");
+	document.body.appendChild(textarea);
+	textarea.value = text;
+	textarea.select();
+	document.execCommand("copy");
+	document.body.removeChild(textarea);
+	textarea = null;
+};
---
+++ chrome-extension/copy_html.js
@@ -0,0 +1,14 @@
+{
+	const selection = window.getSelection();
+	const container = document.createElement("div");
+	for (let i = 0, len = selection.rangeCount; i < len; i++) {
+		container.appendChild(selection.getRangeAt(i).cloneContents());
+	}
+	const selectedHtml = container.innerHTML;
+
+	chrome.runtime.sendMessage({
+		method: "copy",
+		type: "html",
+		value: selectedHtml
+	});
+}
