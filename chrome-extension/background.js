4d01b5730915ec4a454080a3797dd4901f3e459a
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2017-05-18 23:35:06 +0900

    [Update] contextmenuの作成＆選択時の処理を汎用的な構成に変更

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -4,13 +4,19 @@ const generateContextMenuId = (() => {
 	return () => String(count++);
 })();
 
-const CONTEXT_MENU_ID_COPY_HTML = generateContextMenuId();
+const menus = [{
+	id: generateContextMenuId(),
+	title: "選択範囲のHTMLをコピー",
+	contentScriptFile: "/copy_html.js"
+}];
 
 function createContextMenus() {
-	chrome.contextMenus.create({
-		title: "選択範囲のHTMLをコピー",
-		contexts: ["selection"],
-		id: CONTEXT_MENU_ID_COPY_HTML
+	menus.forEach(menu => {
+		chrome.contextMenus.create({
+			title: menu.title,
+			contexts: ["selection"],
+			id: menu.id
+		});
 	});
 }
 
@@ -18,10 +24,11 @@ chrome.runtime.onInstalled.addListener(createContextMenus);
 chrome.runtime.onStartup.addListener(createContextMenus);
 
 chrome.contextMenus.onClicked.addListener(info => {
-	if (info.menuItemId === CONTEXT_MENU_ID_COPY_HTML) {
+	const matchedMenu = menus.find(menu => info.menuItemId === menu.id);
+	if (matchedMenu) {
 		chrome.tabs.executeScript({
 			frameId: info.frameId,
-			file: "/copy_html.js"
+			file: matchedMenu.contentScriptFile
 		});
 	}
 });
