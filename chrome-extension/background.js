5f48c75a45a613f531ebaf9f9cbf47584630cf87
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2017-09-19 22:49:02 +0900

    [Clean] 引用符をダブル(") -> シングル(') に変更

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -6,23 +6,23 @@ const generateContextMenuId = (() => {
 
 const menus = [{
 	id: generateContextMenuId(),
-	title: "選択範囲をMarkdown書式でコピー",
-	contentScriptFile: "/copy_by_markdown_format.js"
+	title: '選択範囲をMarkdown書式でコピー',
+	contentScriptFile: '/copy_by_markdown_format.js'
 }, {
 	id: generateContextMenuId(),
-	title: "選択範囲のHTMLをコピー",
-	contentScriptFile: "/copy_html.js"
+	title: '選択範囲のHTMLをコピー',
+	contentScriptFile: '/copy_html.js'
 }, {
 	id: generateContextMenuId(),
-	title: "選択範囲をBacklog書式でコピー",
-	contentScriptFile: "/copy_by_backlog_format.js"
+	title: '選択範囲をBacklog書式でコピー',
+	contentScriptFile: '/copy_by_backlog_format.js'
 }];
 
 function createContextMenus() {
 	menus.forEach(menu => {
 		chrome.contextMenus.create({
 			title: menu.title,
-			contexts: ["selection"],
+			contexts: ['selection'],
 			id: menu.id
 		});
 	});
@@ -42,17 +42,17 @@ chrome.contextMenus.onClicked.addListener(info => {
 });
 
 chrome.runtime.onMessage.addListener(({method, value}) => {
-	if (method === "copy") {
+	if (method === 'copy') {
 		copy(value);
 	}
 });
 
 const copy = text => {
-	let textarea = document.createElement("textarea");
+	let textarea = document.createElement('textarea');
 	document.body.appendChild(textarea);
 	textarea.value = text;
 	textarea.select();
-	document.execCommand("copy");
+	document.execCommand('copy');
 	document.body.removeChild(textarea);
 	textarea = null;
 };
